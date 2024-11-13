9ca6a46ce3f411f0cfa7791690fd62e9b75c32e1
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-13 13:54:41 +0000

    DOM Selectors

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 // import "./style.css";
+import { Input } from "postcss";
 import "./index.css";
+
+// MARK: DOM Selectors
+const yearEL = document.querySelector("date");
+const inputEL = document.querySelector("input");
+const formEl = document.querySelector("form");
+
+// MARK: Event Listeners
+formEl.addEventListener("submit", (e) => {
+  // Prevent default form submission (turant refesh page hone se rokt hai)
+  e.preventDefault();
+  console.log(inputEL.value);
+});
+
+// MARK: Empty Input
+input.value = "";
+
+
+
+// Get new date function
+// (() => {
+//   const date = new Date();
+//   const year = date.getFullYear();
+//   yearEL.textContent = year;
+// })();
